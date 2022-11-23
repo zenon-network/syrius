fa2b4c0f77546ecaefa4bf8ec6dc1bb7d180311e
alien <118136375+alienc0der@users.noreply.github.com>
2022-11-23 12:11:25 +0200

    Add multiple addresses

--- lib/widgets/modular_widgets/settings_widgets/addresses.dart
+++ lib/widgets/modular_widgets/settings_widgets/addresses.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
+import 'package:number_selector/number_selector.dart';
 import 'package:provider/provider.dart';
 import 'package:zenon_syrius_wallet_flutter/blocs/settings/account_chain_stats_bloc.dart';
 import 'package:zenon_syrius_wallet_flutter/main.dart';
@@ -80,33 +81,53 @@ class AddressesState extends State<Addresses> {
   }
 
   Widget _getAddAddressWidget() {
+    int numAddrToAdd = 1;
     return InkWell(
       onTap: () {
         setState(() {
-          _futureGenerateNewAddress =
-              AddressUtils.generateNewAddress(callback: () {
-            setState(() {
-              _shouldScrollToTheEnd = true;
-            });
-          });
+          _futureGenerateNewAddress = AddressUtils.generateNewAddress(
+              numAddr: numAddrToAdd,
+              callback: () {
+                setState(() {
+                  _shouldScrollToTheEnd = true;
+                });
+              });
         });
       },
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+            Expanded(
+              flex: 1,
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                child: NumberSelector.plain(
+                  borderColor: AppColors.znnColor,
+                  iconColor: AppColors.znnColor,
+                  dividerColor: AppColors.znnColor,
+                  step: 1,
+                  current: 1,
+                  min: 1,
+                  max: 10,
+                  onUpdate: (val) {
+                    numAddrToAdd = val;
+                  },
+                ),
+              ),
+            ),
             const Icon(
               Icons.add_circle,
               color: AppColors.znnColor,
               size: 20.0,
             ),
-            const SizedBox(
-              width: 5.0,
-            ),
-            Text(
-              'Add new address',
-              style: Theme.of(context).textTheme.bodyText1,
+            Container(
+              padding: const EdgeInsets.only(left: 10, right: 80),
+              child: Text(
+                'Add addresses',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
             ),
           ],
         ),
@@ -115,7 +136,7 @@ class AddressesState extends State<Addresses> {
   }
 
   Widget _getAddresses() {
-    List<Widget> _addresses = kDefaultAddressList
+    List<Widget> addresses = kDefaultAddressList
         .map(
           (e) => Row(
             children: [
@@ -139,9 +160,9 @@ class AddressesState extends State<Addresses> {
       controller: _scrollController,
       key: const PageStorageKey('Addresses list view'),
       shrinkWrap: true,
-      itemCount: _addresses.length,
+      itemCount: addresses.length,
       itemBuilder: (context, index) {
-        return _addresses[index];
+        return addresses[index];
       },
     );
 
