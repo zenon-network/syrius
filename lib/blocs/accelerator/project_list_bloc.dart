09bda9a454a6463d2494cab5a4b9e13ee2a92ed2
CryptoFish <YankeeDoodlePigeon@protonmail.com>
2023-01-19 14:13:54 +0100

    Filter on all owned projects

--- lib/blocs/accelerator/project_list_bloc.dart
+++ lib/blocs/accelerator/project_list_bloc.dart
@@ -143,8 +143,8 @@ class ProjectListBloc with RefreshBlocMixin {
 
   /*
   This method filters the projects according to the following rule:
-  if a user doesn't have a Pillar, then we only show him the active
-  projects
+  if a user doesn't have a Pillar, only show the active
+  projects or all owned projects
    */
   Future<List<Project>> _filterProjectsAccordingToPillarInfo(
       Set<Project> projectList) async {
@@ -156,7 +156,7 @@ class ProjectListBloc with RefreshBlocMixin {
           .where(
             (project) =>
                 project.status == AcceleratorProjectStatus.active ||
-                project.owner.toString() == kSelectedAddress,
+                kDefaultAddressList.contains(project.owner.toString()),
           )
           .toList();
       if (activeProjects.isNotEmpty) {
