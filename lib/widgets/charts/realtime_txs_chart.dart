41c2b521b0492c8c301ee8d372163b9eeeb3511f
CryptoFish <YankeeDoodlePigeon@protonmail.com>
2023-04-05 16:19:23 +0200

    Generate spots on update

--- lib/widgets/charts/realtime_txs_chart.dart
+++ lib/widgets/charts/realtime_txs_chart.dart
@@ -35,6 +35,13 @@ class RealtimeTxsChartState extends State<RealtimeTxsChart> {
   }
 
   @override
+  void didUpdateWidget(RealtimeTxsChart oldWidget) {
+    _znnSpots = _generateZnnSpots();
+    _qsrSpots = _generateQsrSpots();
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return StandardChart(
       yValuesInterval: (_maxTransactionsPerDay + 1) > kNumOfChartLeftSideTitles
