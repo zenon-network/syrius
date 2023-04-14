a944a72bb91d0c527ab8d72a64e171d9b4d3379b
CryptoFish <YankeeDoodlePigeon@protonmail.com>
2023-04-15 00:27:24 +0200

    Fix metadata macos script

--- linux/create-git-metadata.sh
+++ linux/create-git-metadata.sh
@@ -1,13 +1,13 @@
+function escape () {
+    echo $1 | sed "s/'/'\"'\"r'/g"
+}
+
 GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 GIT_COMMIT_HASH=$(git rev-parse HEAD)
 GIT_COMMIT_MESSAGE=$(git log -1 --pretty=%s)
 GIT_COMMIT_DATE=$(git --no-pager log -1 --format="%ai")
 GIT_ORIGIN_URL=$(git config --get remote.origin.url)
 GIT_COMMIT_FILE="../lib/utils/metadata.dart"
-
-escape () {
-    echo $1 | sed "s/'/'\"'\"r'/g"
-}
 
 sed --i '1,5d' $GIT_COMMIT_FILE
 
