dac33c660675391e1fc32192f6f11a5549d5046a
CryptoFish <YankeeDoodlePigeon@protonmail.com>
2023-04-14 16:54:45 +0200

    Escape metadata string variables

--- linux/create-git-metadata.sh
+++ linux/create-git-metadata.sh
@@ -5,10 +5,14 @@ GIT_COMMIT_DATE=$(git --no-pager log -1 --format="%ai")
 GIT_ORIGIN_URL=$(git config --get remote.origin.url)
 GIT_COMMIT_FILE="../lib/utils/metadata.dart"
 
-sed --i '1,5d' $GIT_COMMIT_FILE
+escape () {
+    local retval=${1//\\/\\\\}
+    retval=${retval//\$/\\\$}
+    echo $retval
+}
 
-echo "const String gitBranchName = '$GIT_BRANCH_NAME';" >> $GIT_COMMIT_FILE
-echo "const String gitCommitHash = '$GIT_COMMIT_HASH';" >> $GIT_COMMIT_FILE
-echo "const String gitCommitMessage = '$GIT_COMMIT_MESSAGE';" >> $GIT_COMMIT_FILE
-echo "const String gitCommitDate = '$GIT_COMMIT_DATE';" >> $GIT_COMMIT_FILE
-echo "const String gitOriginUrl = '$GIT_ORIGIN_URL';" >> $GIT_COMMIT_FILE
+echo "const String gitBranchName = '''$( escape "$GIT_BRANCH_NAME" )''';" >> $GIT_COMMIT_FILE
+echo "const String gitCommitHash = '''$GIT_COMMIT_HASH''';" >> $GIT_COMMIT_FILE
+echo "const String gitCommitMessage = '''$( escape "$GIT_COMMIT_MESSAGE" )''';" >> $GIT_COMMIT_FILE
+echo "const String gitCommitDate = '''$GIT_COMMIT_DATE''';" >> $GIT_COMMIT_FILE
+echo "const String gitOriginUrl = '''$( escape "$GIT_ORIGIN_URL" )''';" >> $GIT_COMMIT_FILE
--- windows/CreateGitMetadata.ps1
+++ windows/CreateGitMetadata.ps1
@@ -1,14 +1,20 @@
-$GIT_BRANCH_NAME = git rev-parse --abbrev-ref HEAD
+$GIT_BRANCH_NAME = Escape(git rev-parse --abbrev-ref HEAD);
 $GIT_COMMIT_HASH = git rev-parse HEAD
-$GIT_COMMIT_MESSAGE = git log -1 --pretty=%s
+$GIT_COMMIT_MESSAGE = Escape(git log -1 --pretty=%s);
 $GIT_COMMIT_DATE = git --no-pager log -1 --format="%ai"
-$GIT_ORIGIN_URL = git config --get remote.origin.url
+$GIT_ORIGIN_URL = Escape(git config --get remote.origin.url);
 $GIT_COMMIT_FILE = "${PSScriptRoot}\..\lib\utils\metadata.dart"
 
 Clear-Content $GIT_COMMIT_FILE -Force
 
-Add-Content $GIT_COMMIT_FILE "const String gitBranchName = '$GIT_BRANCH_NAME';"
-Add-Content $GIT_COMMIT_FILE "const String gitCommitHash = '$GIT_COMMIT_HASH';"
-Add-Content $GIT_COMMIT_FILE "const String gitCommitMessage = '$GIT_COMMIT_MESSAGE';"
-Add-Content $GIT_COMMIT_FILE "const String gitCommitDate = '$GIT_COMMIT_DATE';"
-Add-Content $GIT_COMMIT_FILE "const String gitOriginUrl = '$GIT_ORIGIN_URL';"
+Add-Content $GIT_COMMIT_FILE "const String gitBranchName = '''$GIT_BRANCH_NAME''';"
+Add-Content $GIT_COMMIT_FILE "const String gitCommitHash = '''$GIT_COMMIT_HASH''';"
+Add-Content $GIT_COMMIT_FILE "const String gitCommitMessage = '''$GIT_COMMIT_MESSAGE''';"
+Add-Content $GIT_COMMIT_FILE "const String gitCommitDate = '''$GIT_COMMIT_DATE''';"
+Add-Content $GIT_COMMIT_FILE "const String gitOriginUrl = '''$GIT_ORIGIN_URL''';"
+
+Function Escape($String) {
+    $String = $String.Replace('\', '\\');
+    $String = $String.Replace('$', '\$');
+    Return $String
+}
