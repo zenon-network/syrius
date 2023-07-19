98aea8134af7bddee8e842ef7020ae50225ac1b8
alienc0der <118136375+alienc0der@users.noreply.github.com>
2023-07-19 11:54:25 +0300

    fix: wallet_connect_tab_child  import

--- lib/widgets/tab_children_widgets/wallet_connect_tab_child.dart
+++ lib/widgets/tab_children_widgets/wallet_connect_tab_child.dart
@@ -4,7 +4,7 @@ import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/wallet_conne
 import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/wallet_connect_widgets/wallet_connect_pairing_list_card.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/wallet_connect_widgets/wallet_connect_qr_card.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/wallet_connect_widgets/wallet_connect_uri_card.dart';
-import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/wallet_connect_widgets/wallet_connection_session_list_card.dart';
+import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/wallet_connect_widgets/wallet_connect_session_list_card.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/widgets.dart';
 
 class WalletConnectTabChild extends StatelessWidget {
