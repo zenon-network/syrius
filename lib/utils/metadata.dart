ef8c0dff935738348c4bc1d6e834eac172d7aba0
Sol Sanctum <so1_sanctum@pm.me>
2023-08-11 18:35:20 -0400

    v0.0.8

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = '';
-const String gitCommitHash = '';
-const String gitCommitMessage = '';
-const String gitCommitDate = '';
-const String gitOriginUrl = '';
+const String gitBranchName = 'master';
+const String gitCommitHash = '2a86d3b81013133bac655f8495350359fccfab31';
+const String gitCommitMessage = 'Merge pull request #19 from alienc0der/bigint-support';
+const String gitCommitDate = '2023-07-20 18:21:10 +0100';
+const String gitOriginUrl = 'https://github.com/zenon-network/syrius.git';
