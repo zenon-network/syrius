41a571d66ce7dee17a0edfa9c6624dae741ebe9f
alienc0der <118136375+alienc0der@users.noreply.github.com>
2023-09-13 00:03:38 +0300

    fix: double quotes for metadata variables

--- linux/create-git-metadata.sh
+++ linux/create-git-metadata.sh
@@ -7,8 +7,8 @@ GIT_COMMIT_FILE="../lib/utils/metadata.dart"
 
 sed --i '1,5d' $GIT_COMMIT_FILE
 
-echo "const String gitBranchName = '$GIT_BRANCH_NAME';" >> $GIT_COMMIT_FILE
-echo "const String gitCommitHash = '$GIT_COMMIT_HASH';" >> $GIT_COMMIT_FILE
-echo "const String gitCommitMessage = '$GIT_COMMIT_MESSAGE';" >> $GIT_COMMIT_FILE
-echo "const String gitCommitDate = '$GIT_COMMIT_DATE';" >> $GIT_COMMIT_FILE
-echo "const String gitOriginUrl = '$GIT_ORIGIN_URL';" >> $GIT_COMMIT_FILE
+echo "const String gitBranchName = \\\"${GIT_BRANCH_NAME}\\\";" >> $GIT_COMMIT_FILE
+echo "const String gitCommitHash = \\\"${GIT_COMMIT_HASH}\\\";" >> $GIT_COMMIT_FILE
+echo "const String gitCommitMessage = \\\"${GIT_COMMIT_MESSAGE}\\\";" >> $GIT_COMMIT_FILE
+echo "const String gitCommitDate = \\\"${GIT_COMMIT_DATE}\\\";" >> $GIT_COMMIT_FILE
+echo "const String gitOriginUrl = \\\"${GIT_ORIGIN_URL}\\\";" >> $GIT_COMMIT_FILE
--- windows/CreateGitMetadata.ps1
+++ windows/CreateGitMetadata.ps1
@@ -7,8 +7,8 @@ $GIT_COMMIT_FILE = "${PSScriptRoot}\..\lib\utils\metadata.dart"
 
 Clear-Content $GIT_COMMIT_FILE -Force
 
-Add-Content $GIT_COMMIT_FILE "const String gitBranchName = '$GIT_BRANCH_NAME';"
-Add-Content $GIT_COMMIT_FILE "const String gitCommitHash = '$GIT_COMMIT_HASH';"
-Add-Content $GIT_COMMIT_FILE "const String gitCommitMessage = '$GIT_COMMIT_MESSAGE';"
-Add-Content $GIT_COMMIT_FILE "const String gitCommitDate = '$GIT_COMMIT_DATE';"
-Add-Content $GIT_COMMIT_FILE "const String gitOriginUrl = '$GIT_ORIGIN_URL';"
+Add-Content $GIT_COMMIT_FILE "const String gitBranchName = `"${GIT_BRANCH_NAME}`";"
+Add-Content $GIT_COMMIT_FILE "const String gitCommitHash = `"${GIT_COMMIT_HASH}`";"
+Add-Content $GIT_COMMIT_FILE "const String gitCommitMessage = `"${GIT_COMMIT_MESSAGE}`";"
+Add-Content $GIT_COMMIT_FILE "const String gitCommitDate = `"${GIT_COMMIT_DATE}`";"
+Add-Content $GIT_COMMIT_FILE "const String gitOriginUrl = `"${GIT_ORIGIN_URL}`";"
