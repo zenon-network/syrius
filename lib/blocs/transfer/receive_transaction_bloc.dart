c875636e018a45c0eefb4673b24031f80bf32983
CryptoFish <13164589+KingGorrin@users.noreply.github.com>
2024-03-09 18:54:32 +0100

    Fix running auto-receiver  (#76)

--- lib/blocs/transfer/receive_transaction_bloc.dart
+++ lib/blocs/transfer/receive_transaction_bloc.dart
@@ -4,15 +4,12 @@ import 'package:zenon_syrius_wallet_flutter/main.dart';
 import 'package:znn_sdk_dart/znn_sdk_dart.dart';
 
 class ReceiveTransactionBloc extends BaseBloc<AccountBlockTemplate?> {
-  void receiveTransaction(String id, BuildContext context) {
+  void receiveTransaction(String id, BuildContext context) async {
     try {
       addEvent(null);
-      sl<AutoReceiveTxWorker>().autoReceiveTransactionHash(Hash.parse(id))
-      .onError(
-        (error, stackTrace) {
-          addError(error, stackTrace);
-        },
-      );
+      var response = await sl<AutoReceiveTxWorker>()
+          .autoReceiveTransactionHash(Hash.parse(id));
+      addEvent(response);
     } catch (e, stackTrace) {
       addError(e, stackTrace);
     }
