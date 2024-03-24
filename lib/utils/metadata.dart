deb34eab861268289db11c3b2e7efc17cc776a7b
CryptoFish <13164589+KingGorrin@users.noreply.github.com>
2024-03-24 10:43:30 +0100

    Fix metadata file

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = r'release-0.2.0';
-const String gitCommitHash = r'96762124924907a40f55243a9d9bc9eb283e709c';
-const String gitCommitMessage = r'Update ledger test run';
-const String gitCommitDate = r'2024-03-20';
-const String gitOriginUrl = r'https://github.com/zenon-network/syrius';
+const String gitBranchName = '';
+const String gitCommitHash = '';
+const String gitCommitMessage = '';
+const String gitCommitDate = '';
+const String gitOriginUrl = '';
