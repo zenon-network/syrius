76f7f62691f1e967d0ba8f190ac12e4656105b36
alienc0der <118136375+alienc0der@users.noreply.github.com>
2024-03-24 11:15:25 +0200

    fix: MacOS QR camera scanner

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = r'release-0.2.0';
-const String gitCommitHash = r'96762124924907a40f55243a9d9bc9eb283e709c';
-const String gitCommitMessage = r'Update ledger test run';
-const String gitCommitDate = r'2024-03-20';
-const String gitOriginUrl = r'https://github.com/zenon-network/syrius';
+const String gitBranchName = r'bugfix/walletconnect-camera-scanner';
+const String gitCommitHash = r'b705ce4b909a57f9b2bfdd6f339724d537eddf7c';
+const String gitCommitMessage = r'Update';
+const String gitCommitDate = r'2024-03-23';
+const String gitOriginUrl = r'https://github.com/zenon-network/syrius.git';
