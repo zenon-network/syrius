3e12e56def051f7980bcc1ddf79e84e2ad33f454
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-10-23 14:13:01 +0300

    fix: Documentation

--- lib/rearchitecture/dashboard/sentinels/widgets/sentinels_error.dart
+++ lib/rearchitecture/dashboard/sentinels/widgets/sentinels_error.dart
@@ -3,9 +3,9 @@ import 'package:zenon_syrius_wallet_flutter/rearchitecture/dashboard/dashboard.d
 import 'package:zenon_syrius_wallet_flutter/widgets/widgets.dart';
 
 /// A widget associated with the [SentinelsState] when it's status is
-/// [CubitStatus.error] that uses the [SyriusErrorWidget] to display a message
+/// [CubitStatus.error] that uses the [SyriusErrorWidget] to display an error.
 class SentinelsError extends StatelessWidget {
-
+  /// Creates a SentinelsError objects.
   const SentinelsError({required this.error, super.key});
   final Object error;
 
--- lib/rearchitecture/dashboard/sentinels/widgets/sentinels_loading.dart
+++ lib/rearchitecture/dashboard/sentinels/widgets/sentinels_loading.dart
@@ -3,7 +3,8 @@ import 'package:zenon_syrius_wallet_flutter/rearchitecture/dashboard/dashboard.d
 import 'package:zenon_syrius_wallet_flutter/widgets/widgets.dart';
 
 /// A widget associated with the [SentinelsState] when it's status is
-/// [CubitStatus.loading] that uses the [SyriusErrorWidget] to display a message
+/// [CubitStatus.loading] that uses the [SyriusLoadingWidget] to display a
+/// loading indicator.
 class SentinelsLoading extends StatelessWidget {
   /// Creates a SentinelsLoading object.
   const SentinelsLoading({super.key});
