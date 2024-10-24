0d7c7db7eb8af706f8b99232d3384ac72382ec24
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-10-24 13:02:12 +0300

    chore: Redo dashboard.dart exports

--- lib/rearchitecture/dashboard/dashboard.dart
+++ lib/rearchitecture/dashboard/dashboard.dart
@@ -1,12 +1,11 @@
-
-export '../dashboard/balance/balance.dart';
-export '../dashboard/balance_dashboard/balance_dashboard.dart';
-export '../dashboard/delegation/delegation.dart';
-export '../dashboard/dual_coin_stats/dual_coin_stats.dart';
-export '../dashboard/pillars/pillars.dart';
-export '../dashboard/realtime_statistics/realtime_statistics.dart';
-export '../dashboard/sentinels/sentinels.dart';
-export '../dashboard/staking/staking.dart';
-export '../dashboard/total_hourly_transactions/total_hourly_transactions.dart';
+export 'balance/balance.dart';
+export 'balance_dashboard/balance_dashboard.dart';
 export 'dashboard_cubit.dart';
+export 'delegation/delegation.dart';
+export 'dual_coin_stats/dual_coin_stats.dart';
+export 'pillars/pillars.dart';
+export 'realtime_statistics/realtime_statistics.dart';
+export 'sentinels/sentinels.dart';
+export 'staking/staking.dart';
+export 'total_hourly_transactions/total_hourly_transactions.dart';
 export 'transfer/transfer.dart';
