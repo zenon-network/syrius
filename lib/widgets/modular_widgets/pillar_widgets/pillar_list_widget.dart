7e81098aeb4825486489e52845e0a48e8eef23e8
kossmmos <184493470+kossmmos@users.noreply.github.com>
2024-11-07 16:09:09 +0200

    changed localization key delegate to delegateKey

--- lib/widgets/modular_widgets/pillar_widgets/pillar_list_widget.dart
+++ lib/widgets/modular_widgets/pillar_widgets/pillar_list_widget.dart
@@ -362,7 +362,7 @@ class _PillarListWidgetState extends State<PillarListWidget> {
         });
         model.delegateToPillar(pillarInfo.name);
       },
-      text: context.l10n.delegate,
+      text: context.l10n.delegateKey,
       textStyle: Theme.of(context).textTheme.titleSmall!.copyWith(
             color: Theme.of(context).textTheme.bodyLarge!.color,
           ),
