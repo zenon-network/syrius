e5a4a40701344c9ce9b0b31f0c3d014eb7b389be
kossmmos <184493470+kossmmos@users.noreply.github.com>
2024-11-09 14:52:50 +0200

    feat: class made serializable

--- lib/model/pillars_qsr_info.dart
+++ lib/model/pillars_qsr_info.dart
@@ -1,9 +1,23 @@
 class PillarsQsrInfo {
-
   PillarsQsrInfo({
     required this.cost,
     required this.deposit,
   });
+
+  factory PillarsQsrInfo.fromJson(Map<String, dynamic> json) {
+    return PillarsQsrInfo(
+      cost: BigInt.parse(json['cost'] as String),
+      deposit: BigInt.parse(json['deposit'] as String),
+    );
+  }
+
   final BigInt cost;
   final BigInt deposit;
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'cost': cost.toString(),
+      'deposit': deposit.toString(),
+    };
+  }
 }
