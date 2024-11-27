0222e4c64ef480665714a817c15a6f70ff1a52d4
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-11-27 19:37:30 +0200

    fix: Dart analysis hint

--- lib/rearchitecture/utils/widgets/widgets.dart
+++ lib/rearchitecture/utils/widgets/widgets.dart
@@ -1,6 +1,5 @@
 export './dropdowns/dropdowns.dart';
+export './infinite_scroll_table/infinite_scroll_table.dart';
 export 'card_scaffold_header.dart';
 export 'card_scaffold_password_field.dart';
-export 'infinite_scroll_table/infinite_scroll_table.dart';
 export 'new_card_scaffold.dart';
-
