87863b0b877db0e905a49ab91be00853dba384db
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-11-29 15:36:39 +0200

    feat: Enable searching and sorting the dropdown items

--- lib/rearchitecture/utils/widgets/dropdowns/zts_dropdown.dart
+++ lib/rearchitecture/utils/widgets/dropdowns/zts_dropdown.dart
@@ -3,7 +3,7 @@ import 'package:zenon_syrius_wallet_flutter/utils/utils.dart';
 import 'package:znn_sdk_dart/znn_sdk_dart.dart';
 
 /// A dropdown for all the network assets - tokens and coins
-class ZtsDropdown extends StatelessWidget {
+class ZtsDropdown extends StatefulWidget {
   /// Creates a new instance.
   const ZtsDropdown({
     required List<Token> availableTokens,
@@ -19,8 +19,15 @@ class ZtsDropdown extends StatelessWidget {
   final List<Token> _availableTokens;
 
   @override
+  State<ZtsDropdown> createState() => _ZtsDropdownState();
+}
+
+class _ZtsDropdownState extends State<ZtsDropdown> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    final List<DropdownMenuEntry<Token>> entries = _availableTokens
+    final List<DropdownMenuEntry<Token>> entries = widget._availableTokens
         .map(
           (Token token) => DropdownMenuEntry<Token>(
             label: token.name,
@@ -32,21 +39,30 @@ class ZtsDropdown extends StatelessWidget {
         )
         .toList();
 
-    final Color color = ColorUtils.getTokenColor(_selectedToken.tokenStandard);
+    final Color color = ColorUtils.getTokenColor(
+      widget._selectedToken.tokenStandard,
+    );
 
     return DropdownMenu<Token>(
+      controller: _searchController,
+      enableFilter: true,
       expandedInsets: EdgeInsets.zero,
-      initialSelection: _selectedToken,
+      filterCallback: _filterCallback,
+      initialSelection: widget._selectedToken,
       inputDecorationTheme: const InputDecorationTheme(
         filled: true,
+      ),
+      leadingIcon: Icon(
+        Icons.search,
+        color: color,
       ),
       dropdownMenuEntries: entries,
       onSelected: (Token? token) {
         if (token != null) {
-          _onChangeCallback(token);
+          widget._onChangeCallback(token);
         }
       },
-      requestFocusOnTap: false,
+      searchCallback: _searchCallback,
       textStyle: TextStyle(
         color: color,
       ),
@@ -56,4 +72,37 @@ class ZtsDropdown extends StatelessWidget {
       ),
     );
   }
+
+  int? _searchCallback(List<DropdownMenuEntry<Token>> entries, String query) {
+    final String searchText = query.toLowerCase();
+    if (searchText.isEmpty) {
+      return null;
+    }
+    final int index = entries.indexWhere(
+      (DropdownMenuEntry<Token> entry) => _matchTest(entry, searchText),
+    );
+
+    return index != -1 ? index : null;
+  }
+
+  List<DropdownMenuEntry<Token>> _filterCallback(
+    List<DropdownMenuEntry<Token>> entries,
+    String filter,
+  ) {
+    final String searchText = filter.toLowerCase();
+    if (searchText.isEmpty) {
+      return entries;
+    }
+
+    final Iterable<DropdownMenuEntry<Token>> filtered = entries.where(
+      (DropdownMenuEntry<Token> entry) => _matchTest(entry, searchText),
+    );
+
+    return filtered.toList();
+  }
+
+  bool _matchTest(DropdownMenuEntry<Token> entry, String searchText) =>
+      entry.label.toLowerCase().contains(searchText) ||
+      entry.value.symbol.toLowerCase().contains(searchText) ||
+      entry.value.tokenStandard.toString().toLowerCase().contains(searchText);
 }
