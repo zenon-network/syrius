d5d2898c90be8038f0f259f15aa3b5ccde80851e
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-12-04 19:02:44 +0200

    fix: Fetching the dlls that will be installed in the Windows app folder and re-enable the generation of git metadata

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = '';
-const String gitCommitHash = '';
-const String gitCommitMessage = '';
-const String gitCommitDate = '';
-const String gitOriginUrl = '';
+const String gitBranchName = r'rearchitecture-dashboard';
+const String gitCommitHash = r'3508666a8cb17c1debbd980a85e1cc7767b6fc6b';
+const String gitCommitMessage = r'feat: Restore updating Git metadata';
+const String gitCommitDate = r'2024-12-02';
+const String gitOriginUrl = r'git@github-maznnwell:maznnwell/syrius.git';
