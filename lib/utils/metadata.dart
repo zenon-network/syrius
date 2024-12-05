2f099081c7fec9e2df7c2e1fe4a8de8c83b5ab8f
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-12-05 15:40:42 +0200

    feat: Update znn sdk and ledger

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = r'rearchitecture-dashboard_macOS';
-const String gitCommitHash = r'a348a32413ddc7df7773531b3d4707ef8cc15a60';
-const String gitCommitMessage = r'shell';
-const String gitCommitDate = r'2024-12-04';
-const String gitOriginUrl = r'git@github.com:kossmmos/syrius.git';
+const String gitBranchName = r'rearchitecture-dashboard';
+const String gitCommitHash = r'ac78ca2a1025e3ec6838ea6a2b119018fad0cb54';
+const String gitCommitMessage = r'fix: Finding the correct path for .so files';
+const String gitCommitDate = r'2024-12-05';
+const String gitOriginUrl = r'git@github-maznnwell:maznnwell/syrius.git';
