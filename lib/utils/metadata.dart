0990cf77de20ab6944753587100ad3c85041472d
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-12-07 20:08:14 +0200

    feat: Reset local git metadata

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = r'rearchitecture-dashboard';
-const String gitCommitHash = r'ac78ca2a1025e3ec6838ea6a2b119018fad0cb54';
-const String gitCommitMessage = r'fix: Finding the correct path for .so files';
-const String gitCommitDate = r'2024-12-05';
-const String gitOriginUrl = r'git@github-maznnwell:maznnwell/syrius.git';
+const String gitBranchName = '';
+const String gitCommitHash = '';
+const String gitCommitMessage = '';
+const String gitCommitDate = '';
+const String gitOriginUrl = '';
