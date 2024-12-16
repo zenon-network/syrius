3a05a6df920fe1c9bdd5de833899c15cf5373340
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-12-16 18:41:52 +0200

    feat: Upgrade dependecies for Flutter 3.27

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = '';
-const String gitCommitHash = '';
-const String gitCommitMessage = '';
-const String gitCommitDate = '';
-const String gitOriginUrl = '';
+const String gitBranchName = r'rearchitecture-transfer';
+const String gitCommitHash = r'b86eaca6de85b513e394ac79ee3a9c0e0a2ad3c0';
+const String gitCommitMessage = r'Merge branch '"'"r'rearchitecture-dashboard'"'"r' into rearchitecture-transfer';
+const String gitCommitDate = r'2024-12-16';
+const String gitOriginUrl = r'git@github-maznnwell:maznnwell/syrius.git';
