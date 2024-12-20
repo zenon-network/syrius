20e375f865720ea1ae265291c3d8b639a358eb98
maznnwell <181888996+maznnwell@users.noreply.github.com>
2024-12-20 20:11:34 +0200

    feat: Remove local Git metadata

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = r'rearchitecture-transfer';
-const String gitCommitHash = r'2305656bc4133cf7201fe7f0414e8efd6057df7d';
-const String gitCommitMessage = r'fix: Errors after merge';
-const String gitCommitDate = r'2024-12-16';
-const String gitOriginUrl = r'git@github-maznnwell:maznnwell/syrius.git';
+const String gitBranchName = '';
+const String gitCommitHash = '';
+const String gitCommitMessage = '';
+const String gitCommitDate = '';
+const String gitOriginUrl = '';
