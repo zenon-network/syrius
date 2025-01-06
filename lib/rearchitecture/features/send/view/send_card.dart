e99ab4e3eb8f13a2e35a579355b46207b8530b93
maznnwell <181888996+maznnwell@users.noreply.github.com>
2025-01-06 21:28:18 +0200

    feat: Add support for refreshing the 'Send' card data

--- lib/rearchitecture/features/send/view/send_card.dart
+++ lib/rearchitecture/features/send/view/send_card.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:zenon_syrius_wallet_flutter/main.dart';
 import 'package:zenon_syrius_wallet_flutter/rearchitecture/features/features.dart';
 import 'package:zenon_syrius_wallet_flutter/rearchitecture/utils/utils.dart';
+import 'package:zenon_syrius_wallet_flutter/utils/utils.dart';
 
 /// A widget to be used along with `Receive` card.
 class SendCard extends StatelessWidget {
@@ -12,6 +14,13 @@ class SendCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return NewCardScaffold(
       data: CardType.send.getData(context: context),
+      onRefreshPressed: () {
+        sl.get<MultipleBalanceBloc>().add(
+          MultipleBalanceFetch(
+            addresses: kDefaultAddressList.map((String? e) => e!).toList(),
+          ),
+        );
+      },
       body: BlocBuilder<MultipleBalanceBloc, MultipleBalanceState>(
         builder: (_, MultipleBalanceState state) => switch (state.status) {
           MultipleBalanceStatus.failure => SendError(error: state.error!),
