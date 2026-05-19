eb02369498aad4e05f747ae347ded2ed2fe354fb
maznnwell <181888996+maznnwell@users.noreply.github.com>
2026-05-19 00:51:54 -0700

    refactor: clear metadata

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = r'rearchitecture-pillars';
-const String gitCommitHash = r'4621e818c9be657692a91e391be00e48c019c494';
-const String gitCommitMessage = r'feat:';
-const String gitCommitDate = r'2026-05-26';
-const String gitOriginUrl = r'git@github-maznnwell:maznnwell/syrius.git';
+const String gitBranchName = r'';
+const String gitCommitHash = r'';
+const String gitCommitMessage = r'';
+const String gitCommitDate = r'';
+const String gitOriginUrl = r'';
