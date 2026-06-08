407b515f1c05532f8e051cab0b565e88b2c23dac
maznnwell <181888996+maznnwell@users.noreply.github.com>
2026-06-08 03:11:51 -0700

    refactor: extract the QSR management step to a separated widget

--- lib/rearchitecture/features/create_pillar_stepper/view/create_pillar_stepper_view.dart
+++ lib/rearchitecture/features/create_pillar_stepper/view/create_pillar_stepper_view.dart
@@ -1,23 +1,18 @@
 import 'dart:async';
 
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:zenon_syrius_wallet_flutter/main.dart';
 import 'package:zenon_syrius_wallet_flutter/rearchitecture/features/create_pillar_stepper/view/widgets/deploy_pillar_step.dart';
 import 'package:zenon_syrius_wallet_flutter/rearchitecture/features/create_pillar_stepper/view/widgets/pillar_plasma_check_step.dart';
+import 'package:zenon_syrius_wallet_flutter/rearchitecture/features/create_pillar_stepper/view/widgets/pillar_qsr_management_step.dart';
 import 'package:zenon_syrius_wallet_flutter/rearchitecture/features/create_pillar_stepper/view/widgets/pillar_registered_success.dart';
 import 'package:zenon_syrius_wallet_flutter/rearchitecture/features/create_pillar_stepper/view/widgets/pillar_znn_management_step.dart';
 import 'package:zenon_syrius_wallet_flutter/rearchitecture/features/features.dart';
 import 'package:zenon_syrius_wallet_flutter/rearchitecture/utils/utils.dart';
-import 'package:zenon_syrius_wallet_flutter/utils/app_colors.dart';
-import 'package:zenon_syrius_wallet_flutter/utils/constants.dart';
 import 'package:zenon_syrius_wallet_flutter/utils/extensions.dart';
-import 'package:zenon_syrius_wallet_flutter/utils/format_utils.dart';
 import 'package:zenon_syrius_wallet_flutter/utils/global.dart';
 import 'package:zenon_syrius_wallet_flutter/utils/input_validators.dart';
-import 'package:zenon_syrius_wallet_flutter/utils/math_utils.dart';
-import 'package:zenon_syrius_wallet_flutter/utils/notification_utils.dart';
 import 'package:zenon_syrius_wallet_flutter/utils/zts_utils.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/reusable_widgets/custom_material_stepper.dart'
     as custom_material_stepper;
@@ -74,18 +69,6 @@ class _MainPillarState extends State<CreatePillarStepperView> {
       InputValidators.validatePillarMomentumAddress(
         _pillarMomentumController.text,
       );
-
-  final GlobalKey<LoadingButtonState> _depositQsrButtonKey = GlobalKey();
-  final GlobalKey<LoadingButtonState> _withdrawButtonKey = GlobalKey();
-
-  /// The minimum value between available and needed QSR to cover the cost
-  ///
-  /// For example: if 100 is available to be deposited, and only 25 needed,
-  /// then this variable is equal to 25
-  ///
-  /// If 100 more QSR is needed, and 100 is available, then variable is equal
-  /// to 100
-  BigInt _maxQsrAmount = BigInt.zero;
 
   double _momentumRewardPercentageGiven = 0;
   double _delegateRewardPercentageGiven = 0;
@@ -161,11 +144,11 @@ class _MainPillarState extends State<CreatePillarStepperView> {
     required AccountInfo accountInfo,
     required _PillarStepperStep? currentStep,
   }) {
-
     // TODO(maznnwell): to be extracted to StepperUtils
     custom_material_stepper.StepState getStepState(
-        _PillarStepperStep step, _PillarStepperStep? currentStep)
-    {
+      _PillarStepperStep step,
+      _PillarStepperStep? currentStep,
+    ) {
       return step.index < (currentStep?.index ?? -1)
           ? custom_material_stepper.StepState.complete
           : custom_material_stepper.StepState.indexed;
@@ -190,7 +173,12 @@ class _MainPillarState extends State<CreatePillarStepperView> {
         ),
         StepperUtils.getMaterialStep(
           stepTitle: context.l10n.management(kQsrCoin.symbol),
-          stepContent: _buildQsrManagementStep(context, accountInfo),
+          stepContent: PillarQsrManagementStep(
+            accountInfo: accountInfo,
+            addressController: _addressController,
+            onNextPressed: _navigateToNextStep,
+            qsrAmountController: _qsrAmountController,
+          ),
           stepSubtitle: context.l10n.deposited(kQsrCoin.symbol),
           stepState: getStepState(
             _PillarStepperStep.qsrManagement,
@@ -253,279 +241,6 @@ class _MainPillarState extends State<CreatePillarStepperView> {
     );
   }
 
-  Widget _buildQsrManagementStep(
-    BuildContext context,
-    AccountInfo accountInfo,
-  ) {
-    return BlocConsumer<
-      CreatePillarQsrInfoBloc,
-      FetchState<CreatePillarQsrInfoData>
-    >(
-      builder: (_, FetchState<CreatePillarQsrInfoData> state) =>
-          switch (state) {
-            FetchFailure<CreatePillarQsrInfoData>() => SyriusErrorWidget(
-              state.exception,
-            ),
-            FetchInitial<CreatePillarQsrInfoData>() => const Padding(
-              padding: EdgeInsets.all(8),
-              child: SyriusLoadingWidget(),
-            ),
-            FetchPopulated<CreatePillarQsrInfoData>() =>
-              _buildQsrManagementStepBody(
-                context,
-                accountInfo,
-                state.data,
-              ),
-          },
-      listener: (_, FetchState<CreatePillarQsrInfoData> state) {
-        if (state is FetchPopulated<CreatePillarQsrInfoData>) {
-          final CreatePillarQsrInfoData data = state.data;
-
-          _maxQsrAmount = MathUtils.bigMin(
-            accountInfo.getBalance(
-              kQsrCoin.tokenStandard,
-            ),
-            MathUtils.bigMax(BigInt.zero, data.cost - data.deposit),
-          );
-          setState(() {
-            _qsrAmountController.text = _maxQsrAmount.addDecimals(
-              coinDecimals,
-            );
-          });
-        }
-      },
-    );
-  }
-
-  Row _buildQsrManagementStepBody(
-    BuildContext context,
-    AccountInfo accountInfo,
-    CreatePillarQsrInfoData qsrInfo,
-  ) {
-    final bool qsrCostCovered = qsrInfo.deposit >= qsrInfo.cost;
-
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  DisabledAddressField(_addressController),
-                  kVerticalSpacing,
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        AvailableBalance(
-                          kQsrCoin,
-                          accountInfo,
-                        ),
-                        Text(
-                          context.l10n.requiredForPillarSlot(
-                            qsrInfo.cost.addDecimals(coinDecimals),
-                            kQsrCoin.symbol,
-                          ),
-                          style: Theme.of(
-                            context,
-                          ).inputDecorationTheme.hintStyle,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Visibility(
-                    visible: !qsrCostCovered,
-                    child: Column(
-                      children: <Widget>[
-                        kVerticalSpacing,
-                        TextFormField(
-                          autovalidateMode: AutovalidateMode.always,
-                          controller: _qsrAmountController,
-                          cursorColor: AppColors.qsrColor,
-                          decoration: InputDecoration(
-                            border: const OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: AppColors.qsrColor,
-                              ),
-                            ),
-                            enabledBorder: const OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: AppColors.qsrColor,
-                              ),
-                            ),
-                            focusedBorder: const OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: AppColors.qsrColor,
-                              ),
-                            ),
-                            hintText: context.l10n.amount,
-                          ),
-                          inputFormatters:
-                              FormatUtils.getAmountTextInputFormatters(
-                                _qsrAmountController.text,
-                              ),
-                          style: const TextStyle(
-                            color: AppColors.qsrColor,
-                          ),
-                          validator: _qsrAmountValidator,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 25),
-                    child: DottedBorderInfoWidget(
-                      borderColor: AppColors.qsrColor,
-                      text: context.l10n.depositedCoinWillBurn(kQsrCoin.symbol),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                children: <Widget>[
-                  Visibility(
-                    visible: !qsrCostCovered,
-                    child: _buildDepositQsrButton(accountInfo, qsrInfo),
-                  ),
-                  Visibility(
-                    visible: qsrCostCovered,
-                    child: OutlinedButton(
-                      onPressed: _navigateToNextStep,
-                      child: Text(context.l10n.next),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-        const SizedBox(
-          width: 45,
-        ),
-        Expanded(
-          child: Visibility(
-            visible: qsrInfo.deposit > BigInt.zero,
-            child: Card.filled(
-              color: context.themeData.inputDecorationTheme.fillColor,
-              child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Expanded(
-                      child: Column(
-                        children: <Widget>[
-                          Stack(
-                            alignment: Alignment.center,
-                            children: <Widget>[
-                              AspectRatio(
-                                aspectRatio: 1,
-                                child: StandardPieChart(
-                                  sections: <PieChartSectionData>[
-                                    PieChartSectionData(
-                                      showTitle: false,
-                                      radius: 7,
-                                      value:
-                                          (qsrInfo.cost - qsrInfo.deposit) /
-                                          qsrInfo.cost,
-                                      color: AppColors.qsrColor.withAlpha(
-                                        (255 * 0.3).round(),
-                                      ),
-                                    ),
-                                    PieChartSectionData(
-                                      showTitle: false,
-                                      radius: 7,
-                                      value: qsrInfo.deposit / qsrInfo.cost,
-                                      color: AppColors.qsrColor,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.all(8),
-                                child: Text(
-                                  context.l10n.currentPillarSlotFee(
-                                    qsrInfo.cost.addDecimals(coinDecimals),
-                                    kQsrCoin.symbol,
-                                  ),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Expanded(
-                      flex: 2,
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            context.l10n.youHaveDeposited(
-                              qsrInfo.deposit.addDecimals(coinDecimals),
-                              kQsrCoin.symbol,
-                            ),
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context).textTheme.bodyLarge,
-                          ),
-                          kVerticalSpacing,
-                          _buildWithdrawQsrButton(
-                            qsrInfo.deposit,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-
-  Widget _buildDepositQsrButton(
-    AccountInfo accountInfo,
-    CreatePillarQsrInfoData qsrInfo,
-  ) {
-    return BlocListener<PillarDepositQsrBloc, PillarDepositQsrState>(
-      listener: (_, PillarDepositQsrState state) {
-        if (state is PillarDepositQsrDone) {
-          _depositQsrButtonKey.currentState?.animateReverse();
-          _refreshPillarQsrInfo();
-        } else if (state is PillarDepositQsrLoading) {
-          _depositQsrButtonKey.currentState?.animateForward();
-        } else if (state is PillarDepositQsrFailure) {
-          _depositQsrButtonKey.currentState?.animateReverse();
-          unawaited(
-            NotificationUtils.sendNotificationError(
-              state.exception,
-              context.l10n.errorWhileDepositing(kQsrCoin.symbol),
-            ),
-          );
-        }
-      },
-      child: LoadingButton(
-        key: _depositQsrButtonKey,
-        text: context.l10n.deposit,
-        onPressed:
-            _hasQsrBalance(accountInfo) &&
-                _qsrAmountValidator(_qsrAmountController.text) == null
-            ? () => _onDepositButtonPressed(qsrInfo)
-            : null,
-        outlineColor: AppColors.qsrColor,
-        textStyle: const TextStyle(
-          color: AppColors.qsrColor,
-        ),
-      ),
-    );
-  }
-
   void _refreshPillarQsrInfo() {
     context.read<CreatePillarQsrInfoBloc>().add(
       FetchRequestData(
@@ -534,67 +249,9 @@ class _MainPillarState extends State<CreatePillarStepperView> {
     );
   }
 
-  Widget _buildWithdrawQsrButton(
-    BigInt qsrDeposit,
-  ) {
-    return BlocListener<PillarWithdrawQsrBloc, PillarWithdrawQsrState>(
-      listener: (_, PillarWithdrawQsrState state) {
-        if (state is PillarWithdrawQsrLoading) {
-          _withdrawButtonKey.currentState?.animateForward();
-        } else if (state is PillarWithdrawQsrFailure) {
-          _withdrawButtonKey.currentState?.animateReverse();
-          unawaited(
-            NotificationUtils.sendNotificationError(
-              state.exception,
-              context.l10n.errorWhileWithdrawing(kQsrCoin.symbol),
-            ),
-          );
-        } else if (state is PillarWithdrawQsrPopulated) {
-          _withdrawButtonKey.currentState?.animateReverse();
-          _navigateToNextStep();
-          _refreshPillarQsrInfo();
-        }
-      },
-      child: LoadingButton(
-        text: context.l10n.withdraw,
-        onPressed: () => _onWithdrawButtonPressed(qsrDeposit),
-        key: _withdrawButtonKey,
-        outlineColor: AppColors.qsrColor,
-        textStyle: const TextStyle(
-          color: AppColors.qsrColor,
-        ),
-      ),
-    );
-  }
-
   void _onDeployDone() {
     // All steps have been completed
     _currentStep.value = null;
-  }
-
-  void _onDepositButtonPressed(
-    CreatePillarQsrInfoData qsrInfo,
-  ) {
-    final BigInt qsrAmount = _qsrAmountController.text.extractDecimals(
-      coinDecimals,
-    );
-
-    final bool isQsrAvailableToDeposit = qsrAmount > BigInt.zero;
-
-    final bool willDepositExceedCost =
-        qsrInfo.deposit + qsrAmount > qsrInfo.cost;
-
-    final bool canDepositBeExecuted =
-        isQsrAvailableToDeposit && !willDepositExceedCost;
-
-    if (canDepositBeExecuted) {
-      context.read<PillarDepositQsrBloc>().add(
-        PillarDepositQsrRequested(
-          address: Address.parse(_addressController.text),
-          amount: qsrAmount,
-        ),
-      );
-    }
   }
 
   void _onDeployPressed() {
@@ -609,18 +266,6 @@ class _MainPillarState extends State<CreatePillarStepperView> {
         giveDelegateRewardPercentage: _delegateRewardPercentageGiven.toInt(),
       ),
     );
-  }
-
-  void _onWithdrawButtonPressed(
-    BigInt qsrDeposit,
-  ) {
-    if (qsrDeposit > BigInt.zero) {
-      context.read<PillarWithdrawQsrBloc>().add(
-        PillarWithdrawQsrRequested(
-          address: Address.parse(_addressController.text),
-        ),
-      );
-    }
   }
 
   Future<void> _onDeployAnotherPillarButtonPressed() async {
@@ -642,17 +287,6 @@ class _MainPillarState extends State<CreatePillarStepperView> {
       _pillarNameError == null &&
       _pillarRewardAddressError == null &&
       _pillarMomentumError == null;
-
-  bool _hasQsrBalance(AccountInfo accountInfo) =>
-      accountInfo.qsr()! > BigInt.zero;
-
-  String? _qsrAmountValidator(String? value) => InputValidators.correctValue(
-    value,
-    _maxQsrAmount,
-    kQsrCoin.decimals,
-    BigInt.one,
-    canBeEqualToMin: true,
-  );
 
   void _onPlasmaCheckNextPressed() {
     _navigateToNextStep();
