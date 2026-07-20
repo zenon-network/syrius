391ff596e084e94a7f879bad79c59185e66d7d61
maznnwell <181888996+maznnwell@users.noreply.github.com>
2026-07-20 13:21:08 -0700

    feat: improve button

--- lib/rearchitecture/utils/widgets/buttons/clear_content_button.dart
+++ lib/rearchitecture/utils/widgets/buttons/clear_content_button.dart
@@ -2,15 +2,24 @@ import 'package:flutter/material.dart';
 
 /// A simple button that helps clear content inside a [TextField] or
 /// [TextFormField]
-class ClearContentButton extends IconButton {
-  /// Creates a button that clears [controller].
-  ClearContentButton({
-    required TextEditingController controller,
-    super.key,
-  }) : super(
-         onPressed: controller.clear,
-         icon: const Icon(
-           Icons.clear,
-         ),
-       );
+class ClearContentButton extends StatelessWidget {
+  /// Creates a button that clears [_controller].
+  const ClearContentButton({required this._controller, super.key});
+
+  final TextEditingController _controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<TextEditingValue>(
+      valueListenable: _controller,
+      builder: (_, TextEditingValue value, _) {
+        final bool isActive = value.text.isNotEmpty;
+
+        return IconButton(
+          onPressed: isActive ? _controller.clear : null,
+          icon: const Icon(Icons.clear),
+        );
+      },
+    );
+  }
 }
