1c1a0fe75128faf46c12f348cee18c54447987ab
maznnwell <181888996+maznnwell@users.noreply.github.com>
2026-07-26 04:46:24 -0700

    refactor: reset metadata

--- lib/utils/metadata.dart
+++ lib/utils/metadata.dart
@@ -1,5 +1,5 @@
-const String gitBranchName = r'rearchitecture-tokens';
-const String gitCommitHash = r'bf516b9454f6bd725db63c495a3aaac95de27196';
-const String gitCommitMessage = r'refactor:';
-const String gitCommitDate = r'2026-07-18';
-const String gitOriginUrl = r'git@github-maznnwell:maznnwell/syrius.git';
+const String gitBranchName = r'';
+const String gitCommitHash = r'';
+const String gitCommitMessage = r'';
+const String gitCommitDate = r'';
+const String gitOriginUrl = r'';
